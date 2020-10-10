b69eea19a2937fc13b1e3df8aa09ffa5d7fdd3ac
nobody <a.gaston.gimenez@gmail.com>
2020-10-10 19:19:12 -0300

    Show the available tools first

--- app/toolz.py
+++ app/toolz.py
@@ -4,43 +4,48 @@ import sys
 import os
 from color import Color
 from shutil import which
+from get_tools_per_keyword import get_tools_per_keyword
 
 class Toolz:
     def __init__(self):
-        self.tools_per_service = self.load_tools_per_service()
         self.tools_info = self.load_tools_info()
+        self.tools_per_keyword = get_tools_per_keyword(self.tools_info)
         self.params_cache = {}
      
-    def load_tools_per_service(self):
-        with open('tools_per_keyword.json') as tools:
-            return json.load(tools)
-
     def load_tools_info(self):
         with open('tools_info.json') as tools_info:
-            return json.load(tools_info)
+            tools_info = json.load(tools_info)
+            for tool_name in tools_info:
+                tools_info.get(tool_name)["available"] = which(tool_name) is not None
+            return tools_info
         
-    def get_tools_per_service(self, service):
-        if service not in self.tools_per_service:
+    def get_tools_per_keyword(self, service):
+        if service not in self.tools_per_keyword:
             return
-        return self.tools_per_service[service]
+        return self.tools_per_keyword[service]
 
-    def print_tools_per_service(self, service):
-        self.tools_result_list = self.get_tools_per_service(service)
-        if self.tools_result_list is None:
-            print(f'{Color.YELLOW}\nOuch! No tools found for {service}\n{Color.END}')
+    def print_tools_per_keyword(self, keyword):
+        matches_list = self.get_tools_per_keyword(keyword)
+        if matches_list is None:
+            print(f'{Color.YELLOW}\nOuch! No tools found for keyword {keyword}\n{Color.END}')
             return None
 
-        print(f'{Color.BOLD}\nResults for {service}:{Color.END}')
-        for ind, tool in enumerate(self.tools_result_list):
-            tool_info = self.get_tool_info(tool)
-            available = which(tool) is not None
-            print(f'{ind + 1}) {Color.GREEN if available else Color.RED}{tool}{Color.END}: {tool_info["description"]}')
-
+        print(f'{Color.BOLD}\nResults for {keyword}:{Color.END}')
+        tool_info_list = []
+        for tool_name in matches_list:
+            tool_info_list.append({
+                "name": tool_name,
+                **self.tools_info[tool_name],
+            });
+        sorted_tool_info_list = sorted(tool_info_list, key=lambda t: t["available"], reverse = True)
+        for ind, tool in enumerate(sorted_tool_info_list):
+            print(f'{ind + 1}) {Color.GREEN if tool.get("available") else Color.RED}{tool.get("name")}{Color.END}: {tool.get("description")}')
+        self.tools_result_list = sorted_tool_info_list
         return self.tools_result_list
 
     def print_tool_info(self, tool_ind):
-        self.service_name = self.tools_result_list[tool_ind - 1]
-        self.tool_info = self.get_tool_info(self.service_name)
+        self.tool_info = self.tools_result_list[tool_ind - 1]
+        self.service_name = self.tool_info.get("name")
         examples = self.tool_info["examples"]
         if examples is None or len(examples) == 0:
             print(f'{Color.YELLOW}No examples found for {self.service_name}!!!{Color.END}')
@@ -96,7 +101,7 @@ def main():
     toolz = Toolz()
     tool_ind = None
     while True:
-        found_tools = toolz.print_tools_per_service(search)
+        found_tools = toolz.print_tools_per_keyword(search)
         if not found_tools:
             search = None
             while not search:
@@ -113,7 +118,7 @@ def main():
 
         if not tool_ind:
             old_search = search
-            search = input("Search for tool or service: ")
+            search = input(f'Search for tool or service{" (" + old_search + ")" if old_search else ""}: ')
             if not search:
                search = old_search
             continue
